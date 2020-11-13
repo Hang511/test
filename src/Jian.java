ba65456d3fe167f4a88b7f23799812b54a02884f
Hang511 <1715537876@qq.com>
2020-11-13 23:46:24 +0800

    增加Zhao.java和Jian.java

---
+++ src/Jian.java
@@ -0,0 +1,5 @@
+public class Jian {
+    public static void main(String[] args) {
+        System.out.println("测试代码2");
+    }
+}
---
+++ src/Zhao.java
@@ -0,0 +1,5 @@
+public class Zhao {
+    public static void main(String[] args) {
+        System.out.println("测试代码2");
+    }
+}
